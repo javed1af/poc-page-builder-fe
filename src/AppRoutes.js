a488f24a1d79d7237eb639e61e321a7136db7a87
javed1af <javed1af@gmail.com>
2024-05-26 17:00:36 +0500

    REFACT

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
-import PuckEditor from './components/PuckEditor';
-import MySitePage from './components/MySitePage';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
-      {/* <Route path="/editor" element={<PuckEditor />} /> */}
-      {/* <Route path="/my-site-page" element={<MySitePage />} /> */}
     </Routes>
   );
 };
